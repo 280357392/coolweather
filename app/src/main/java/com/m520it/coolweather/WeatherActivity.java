b84974817679b182fe1a5e5107c5a02c3dfcc430
Tony <yyy333567@163.com>
2018-05-01 23:23:46 +0800

    新增切换城市和手动更新天气功能two

--- app/src/main/java/com/m520it/coolweather/WeatherActivity.java
+++ app/src/main/java/com/m520it/coolweather/WeatherActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -120,7 +121,8 @@ public class WeatherActivity extends AppCompatActivity {
      */
     public void requestWeather(final String weatherId) {
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" +
-                weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+                weatherId + "&key=ffcfc96d70a64a7e915778c651129558 ";
+        Log.e("TAG", "requestWeather: "+weatherUrl);
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
